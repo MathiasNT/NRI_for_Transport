8f11159d35243f0e612298d34ee72ee05f9e5fa5
Mathias Niemann Tygesen <mnity@dtu.dk>
2020-11-16 23:41:18 +0100

    init

--- GraphTrafficLib/models/__init__.py
+++ GraphTrafficLib/models/__init__.py
@@ -2,5 +2,12 @@ from .DMVST_Net import DMVST_Net
 from .lstm_feature_model import LSTM_feature_model
 from .lstm_gnn import LSTM_GNN_feature_model
 from .lstm_model import LSTM_model
+from .lstm_w_conv import LSTM_w_Conv
 
-__all__ = ["DMVST_Net", "LSTM_feature_model", "LSTM_GNN_feature_model", "LSTM_model"]
+__all__ = [
+    "DMVST_Net",
+    "LSTM_feature_model",
+    "LSTM_GNN_feature_model",
+    "LSTM_model",
+    "LSTM_w_Conv",
+]
