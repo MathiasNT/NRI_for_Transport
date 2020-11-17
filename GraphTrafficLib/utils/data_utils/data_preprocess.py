a074b2db3331275bee8f2db4abd43b942a149ad2
Mathias Niemann Tygesen <mnity@dtu.dk>
2020-11-17 23:38:06 +0100

    bug fix

--- GraphTrafficLib/utils/data_utils/data_preprocess.py
+++ GraphTrafficLib/utils/data_utils/data_preprocess.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 
 
 def add_spatial_bins(df, n_lat_bins, n_lon_bins):
-    # This functions add latitude and longitude bins to a dataframe with geo coordinates
+    # This functions add latitude and longitude bins to a dataframe
+    # with geo coordinates
     # Note that the function requires "pickup_x" for it to work
 
     # Add the longitude bins
@@ -11,7 +13,6 @@ def add_spatial_bins(df, n_lat_bins, n_lon_bins):
     )
 
     # Add the latitude bins
-    longitude_bin_coord = pd.cut(df["pickup_longitude"], n_lon_bins).cat.categories
     df.loc[:, "latitude_bins"] = pd.cut(
         df["pickup_latitude"], n_lat_bins, labels=range(n_lat_bins - 1, -1, -1)
     )  # We reverse the latitude bins as latitude increases up, not down
@@ -25,9 +26,9 @@ def add_temporal_bins(df, dt):
     min_date = df.pickup_time.min()
     max_date = df.pickup_time.max()
 
-    # Create the bins. Note that this misses a bit from the beginning but this will not be a big problem when we index finer
+    # Create the bins. Note that this misses a bit from the
+    # beginning but this will not be a big problem when we index finer
     bins_dt = pd.date_range(start=min_date, end=max_date, freq=dt)
-    bins_str = bins_dt.astype(str).values
     n_bins_dt = len(bins_dt) - 1
     print(f"from {min_date} to {max_date}")
 
@@ -41,7 +42,7 @@ def add_temporal_bins(df, dt):
     return df, n_bins_dt
 
 
-def create_binned_matrix(df):
+def create_binned_matrix(df, n_lat_bins, n_lon_bins, n_bins_dt):
     # Group the data based on the spatial bins. Note that the temporal order is in the data already
     location_groups = [x for _, x in df.groupby(["longitude_bins", "latitude_bins"])]
     group_idx = [idx for idx, _ in df.groupby(["longitude_bins", "latitude_bins"])]
@@ -54,4 +55,3 @@ def create_binned_matrix(df):
         binned_matrix[lat, lon, :] = location_time_series.values
 
     return binned_matrix
-
