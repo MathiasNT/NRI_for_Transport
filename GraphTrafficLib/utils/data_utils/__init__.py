5ca51cb9ef6cf50249acb85ab8b87e95ed5f820e
Mathias Niemann Tygesen <mnity@dtu.dk>
2020-11-17 23:15:47 +0100

    init

--- GraphTrafficLib/utils/data_utils/__init__.py
+++ GraphTrafficLib/utils/data_utils/__init__.py
@@ -1,4 +1,9 @@
 from .Nyc_w_Weather import Nyc_w_Weather
-from .data_preprocess import add_spatial_bins, add_temporal_bins
+from .data_preprocess import add_spatial_bins, add_temporal_bins, create_binned_matrix
 
-__all__ = ["Nyc_w_Weather", "add_spatial_bins", "add_temporal_bins"]
+__all__ = [
+    "Nyc_w_Weather",
+    "add_spatial_bins",
+    "add_temporal_bins",
+    "create_binned_matrix",
+]
