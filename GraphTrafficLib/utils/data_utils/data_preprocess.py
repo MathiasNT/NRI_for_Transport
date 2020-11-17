c4949b52b91ad9d2f87a04cab4d59655f09e98f3
Mathias Niemann Tygesen <mnity@dtu.dk>
2020-11-17 23:04:38 +0100

    BUG

--- GraphTrafficLib/utils/data_utils/data_preprocess.py
+++ GraphTrafficLib/utils/data_utils/data_preprocess.py
@@ -2,17 +2,8 @@ import pandas as pd
 
 
 def add_spatial_bins(df, n_lat_bins, n_lon_bins):
-    """This functions add latitude and longitude bins to a dataframe with geo coordinates
-    Note that the function requires "pickup_x" for it to work
-
-    Args:
-        df (pd.Dataframe): The dataframe
-        n_lat_bins (int): number of latitude bins
-        n_lon_bins (int): number of longitude bins
-
-    Returns:
-        pd.DataFrame: Dataframe with added bins
-    """
+    # This functions add latitude and longitude bins to a dataframe with geo coordinates
+    # Note that the function requires "pickup_x" for it to work
 
     # Add the longitude bins
     df.loc[:, "longitude_bins"] = pd.cut(
@@ -28,20 +19,11 @@ def add_spatial_bins(df, n_lat_bins, n_lon_bins):
 
 
 def add_temporal_bins(df, dt):
-    """This function adds temporal bins to a dataframe with timestamps
+    # This function adds temporal bins to a dataframe with timestamps
 
-
-    Args:
-        df (pd.DataFrame): The dataframe
-        dt (string): The frequency to bin into. The string must match a possible pd.date_range freq string
-
-    Returns:
-        pd.DataFrame: The dataframe with added temporal bins
-    """
-    min_date = demand_df.pickup_time.min()
-
-    # Get the max date
-    max_date = demand_df.pickup_time.max()
+    # Get the min and max date
+    min_date = df.pickup_time.min()
+    max_date = df.pickup_time.max()
 
     # Create the bins. Note that this misses a bit from the beginning but this will not be a big problem when we index finer
     bins_dt = pd.date_range(start=min_date, end=max_date, freq=dt)
@@ -50,11 +32,10 @@ def add_temporal_bins(df, dt):
     print(f"from {min_date} to {max_date}")
 
     # Create the temporal bins
-    demand_df.loc[:, "time_bins"] = pd.cut(
+    df.loc[:, "time_bins"] = pd.cut(
         df["pickup_time"], bins=bins_dt, labels=range(n_bins_dt)
     )
 
     # due to the time indexing we have some values that gets outside the bins - here is a temporary fix
-    demand_df = demand_df.dropna()
-    return demand_df
-
+    df = df.dropna()
+    return df
