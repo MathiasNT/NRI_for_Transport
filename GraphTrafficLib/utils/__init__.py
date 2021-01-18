59b13f5916e9764430be7c6a16b1f876e6359489
Mathias Niemann Tygesen <mnity@dtu.dk>
2021-01-18 13:40:18 +0100

    fixed placement

--- GraphTrafficLib/utils/__init__.py
+++ GraphTrafficLib/utils/__init__.py
@@ -1,4 +1,5 @@
 from .data_utils import Nyc_w_Weather
-from .graph_generators import dtw_adj_generator, encode_onehot
+from .graph_generators import dtw_adj_generator
+from .general_utils import encode_onehot
 
 __all__ = ["Nyc_w_Weather", "dtw_adj_generator", "encode_onehot"]
--- GraphTrafficLib/utils/graph_generators/__init__.py
+++ GraphTrafficLib/utils/graph_generators/__init__.py
@@ -1,4 +1,3 @@
 from .adjacancy_matrix_generators import dtw_adj_generator
-from .utils import encode_onehot
 
-__all__ = ["dtw_adj_generator", "encode_onehot"]
+__all__ = ["dtw_adj_generator"]
