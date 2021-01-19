7b3214bd29ac9f4c22d5f2d7874b962c4fe2fcec
Mathias Niemann Tygesen <mnity@dtu.dk>
2021-01-19 15:21:30 +0100

    update init

--- GraphTrafficLib/models/latent_graph/__init__.py
+++ GraphTrafficLib/models/latent_graph/__init__.py
@@ -1,5 +1,5 @@
-from .decoders import MLPDecoder
+from .decoders import MLPDecoder, GRUDecoder
 from .encoders import MLPEncoder
 from .modules import MLP
 
-__all__ = ["MLPDecoder", "MLPEncoder", "MLP"]
+__all__ = ["MLPDecoder", "MLPEncoder", "MLP", "GRUDecoder"]
