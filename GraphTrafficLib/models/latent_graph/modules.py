418448134655ae3799b82d0f4052222ae048a528
Mathias Niemann Tygesen <mnity@dtu.dk>
2021-01-19 11:11:51 +0100

    updated mlp init to avoid depreciation

--- GraphTrafficLib/models/latent_graph/modules.py
+++ GraphTrafficLib/models/latent_graph/modules.py
@@ -20,7 +20,7 @@ class MLP(nn.Module):
     def init_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Linear):
-                nn.init.xavier_normal(m.weight.data)
+                nn.init.xavier_normal_(m.weight.data)
                 m.bias.data.fill_(0.1)
             elif isinstance(m, nn.BatchNorm1d):
                 m.weight.data.fill_(1)
