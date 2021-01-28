2e6dccfe9bbb3d5baa4d4a482390808a3d96217f
Mathias Niemann Tygesen <mnity@dtu.dk>
2021-01-28 09:47:26 +0100

    update init

--- GraphTrafficLib/utils/data_utils/__init__.py
+++ GraphTrafficLib/utils/data_utils/__init__.py
@@ -1,11 +1,11 @@
-from .Nyc_w_Weather import Nyc_w_Weather
+from .Nyc_w_Weather import Nyc_w_Weather, Nyc_w_Weather2
 from .data_preprocess import (
     add_spatial_bins,
     add_temporal_bins,
     create_binned_matrix,
     create_binned_vector,
 )
-from .data_loader_utils import create_test_train_split
+from .data_loader_utils import create_test_train_split, create_test_train_split2
 
 __all__ = [
     "Nyc_w_Weather",
@@ -14,4 +14,6 @@ __all__ = [
     "create_binned_matrix",
     "create_binned_vector",
     "create_test_train_split",
+    "Nyc_w_Weather2",
+    "create_test_train_split2",
 ]
