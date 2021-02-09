1b88d70e9d366a05ca714f3668f753499fb905b5
Mathias Niemann Tygesen <mnity@dtu.dk>
2021-02-09 11:45:34 +0100

    added performance measures

--- GraphTrafficLib/utils/__init__.py
+++ GraphTrafficLib/utils/__init__.py
@@ -1,5 +1,5 @@
 from .data_utils import Nyc_w_Weather
 from .graph_generators import dtw_adj_generator
-from .general_utils import encode_onehot
+from .general_utils import encode_onehot, RMSE, MAE, MAPE
 
-__all__ = ["Nyc_w_Weather", "dtw_adj_generator", "encode_onehot"]
+__all__ = ["Nyc_w_Weather", "dtw_adj_generator", "encode_onehot", "RMSE", "MAE", "MAPE"]
--- GraphTrafficLib/utils/general_utils.py
+++ GraphTrafficLib/utils/general_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 
 def encode_onehot(labels):
@@ -9,3 +10,16 @@ def encode_onehot(labels):
     classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
     labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
     return labels_onehot
+
+
+def RMSE(pred, target):
+    return torch.sqrt(torch.mean((pred - target) ** 2))
+
+
+def MAE(pred, target):
+    return torch.abs(pred - target).mean()
+
+
+def MAPE(pred, target):
+    return torch.abs((target - pred) / target).mean()
+
